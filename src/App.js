1ee057dc97b1b33fd713427ec702f73fca292111
Elchanan Shuky Shukrun <elnn.sh@gmail.com>
2024-09-09 12:13:49 +0300

    fix: mobile view

--- src/App.js
+++ src/App.js
@@ -92,7 +92,7 @@ const App = () => {
     };
 
     return (
-        <div>
+        <div className="container">
             <h1>Trade Game</h1>
             {!gameStarted ? (
                 <div>
